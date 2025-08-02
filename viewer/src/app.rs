753d1d3026f49e6105a3b0e9bb5d64ce33751391
Byron Wasti <byron.wasti@gmail.com>
2025-08-01 20:06:38 -0400

    Egui basics

---
+++ viewer/src/app.rs
@@ -0,0 +1,120 @@
+use egui::{
+    Vec2,
+    Sense,
+    Frame
+};
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct PollinationViewer {
+    label: String,
+
+    #[serde(skip)]
+    value: f32,
+}
+
+impl Default for PollinationViewer {
+    fn default() -> Self {
+        Self {
+            label: "Hello World!".to_owned(),
+            value: 2.7,
+        }
+    }
+}
+
+impl PollinationViewer {
+    /// Must be called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        if let Some(storage) = cc.storage {
+            eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
+        } else {
+            Default::default()
+        }
+    }
+}
+
+impl eframe::App for PollinationViewer {
+    /// Called by the framework to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::MenuBar::new().ui(ui, |ui| {
+                // NOTE: no File->Quit on web pages!
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    ui.allocate_space(ui.available_size());
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::SidePanel::left("Side panel").show(ctx, |ui| {
+            ui.label("Hello world");
+            ui.allocate_space(ui.available_size());
+        });
+
+        egui::SidePanel::right("Right panel").show(ctx, |ui| {
+            ui.label("Settings");
+            ui.allocate_space(ui.available_size());
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("eframe template");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut self.label);
+            });
+
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                self.value += 1.0;
+            }
+
+            Frame::canvas(ui.style()).show(ui, |ui| {
+                let (response, painter) =
+                    ui.allocate_painter(Vec2::new(ui.available_width(), 300.0), Sense::hover());
+                response
+            });
+
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+                egui::warn_if_debug_build(ui);
+            });
+        });
+    }
+}
+
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
+}
