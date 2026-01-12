bd0a59814ba85ac081a9b8fef2ac33b5d3f1b677
Byron Wasti <byron.wasti@gmail.com>
2026-01-11 20:28:56 -0500

    more cleanup

--- pollination-simulator/src/config.rs
+++ pollination-simulator/src/config.rs
@@ -15,31 +15,3 @@ impl<C> Config<C> {
         }
     }
 }
-
-/*
-pub struct StartupConfig {
-    pub node_count: usize,
-    pub seed: u64,
-    pub connections: usize,
-}
-
-pub struct StepConfig {
-    pub timeout_propagativity: u64,
-    pub timeout_heartbeat: u64,
-    pub timeout_reap: u64,
-
-    /// Only used if connections == 0
-    pub rand_robin_count: usize,
-}
-
-impl Default for StepConfig {
-    fn default() -> StepConfig {
-        StepConfig {
-            timeout_propagativity: 13,
-            timeout_heartbeat: 5,
-            timeout_reap: 8,
-            rand_robin_count: 2,
-        }
-    }
-}
-*/
